28ede2b8fa3bcddcf9288110e4ec306bb5bd7413
kbgro <daniendegwa@gmail.com>
2021-11-09 11:22:50 +0300

    add data and log storage settings

--- asianbookie/settings.py
+++ asianbookie/settings.py
@@ -1,16 +1,28 @@
 import logging
+import os
+from pathlib import Path
 
 ASIAN_BOOKIE_URL = "https://tipsters.asianbookie.com"
 ASIAN_BOOKIE_TOP_TIPSTERS_URL = "https://tipsters.asianbookie.com/index.cfm?top20=1"
 
+DATA_DIR = Path(__file__).resolve(True).parent.parent / "data"
+LOG_DIR = Path(__file__).resolve(True).parent.parent / "logs"
+
+
+def setup():
+    setup_data_directory()
+    setup_logger()
+
 
 def setup_logger():
     format_str = "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"
+    log_file = LOG_DIR / "asianbookie.log"
+
     logging.basicConfig(
         level=logging.DEBUG,
         format=format_str,
         datefmt="%m-%d %H:%M",
-        filename="asianbookie.log",
+        filename=log_file,
         filemode="w",
     )
     # define a Handler which writes INFO messages or higher to the sys.stderr
@@ -22,3 +34,8 @@ def setup_logger():
     console.setFormatter(formatter)
     # add the handler to the root logger
     logging.getLogger("").addHandler(console)
+
+
+def setup_data_directory():
+    os.makedirs(DATA_DIR, exist_ok=True)
+    os.makedirs(LOG_DIR, exist_ok=True)
