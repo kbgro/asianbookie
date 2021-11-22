645479fb9e632da6d58b8898a843f4a4d30386ce
kbgro <daniendegwa@gmail.com>
2021-11-22 21:11:01 +0300

    remove tipster subpackage

--- asianbookie/tipsters/__init__.py
+++
@@ -1,4 +0,0 @@
-from .parser import TipsterProfileParser, Top10LeagueTipsterParser, Top100TipsterParser
-from .user import AsianBookieUser
-
-__all__ = [AsianBookieUser, TipsterProfileParser, Top100TipsterParser, Top10LeagueTipsterParser]
