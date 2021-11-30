210ff4a9df7bb1fe83856a1adf139d43491f3741
kbgro <daniendegwa@gmail.com>
2021-11-30 10:48:24 +0300

    Bump version: {3.0.0} -> {3.0.1}

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/kbgro/asianbookie",
-    version="3.0.0",
+    version="3.0.1",
     zip_safe=False,
 )
